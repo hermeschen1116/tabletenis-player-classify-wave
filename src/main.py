acbaf3abdce9e8ca7e0fdac1b0352ec3b20ee1a6
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-03 20:26:33 +0800

    feat: implement model and training script

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import multiprocessing
 import os
+import random
 from typing import List
 
+import numpy
 import polars
 import torch
 from datasets import Dataset
 from dotenv import load_dotenv
-from sklearn.metrics import roc_auc_score
 from torch.nn import CrossEntropyLoss
+from torch.optim.lr_scheduler import ConstantLR
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
@@ -15,9 +17,20 @@ from tabletenis_player_classify_wave.Model import LSTMClassifier
 
 load_dotenv()
 
+seed: int = 42
+random.seed(seed)
+numpy.random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+	torch.cuda.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+
 project_root: str = os.environ.get("PROJECT_ROOT", "")
 num_workers: int = multiprocessing.cpu_count()
 device: str = "cuda"
+dtype: torch.dtype = torch.float32
 features: List[str] = ["Ax", "Ay", "Az", "Gx", "Gy", "Gz"]
 labels: List[str] = ["gender", "hold racket handed", "play years", "level"]
 
@@ -28,32 +41,7 @@ train_data_df = polars.read_csv(
 	f"{project_root}/data/train_data.csv", n_threads=num_workers, low_memory=True, rechunk=True, use_pyarrow=True
 )
 
-train_data_df = (
-	train_data_df.sort("player_ID", "data_id", "time_order")
-	.set_sorted("time_order")
-	.group_by("player_ID", maintain_order=True)
-	.agg([polars.col(features).mean().name.suffix("_mean"), polars.col(features).std().name.suffix("_std")])
-	.join(train_data_df, on="player_ID")
-	.with_columns(((polars.col(col) - polars.col(f"{col}_mean")) / polars.col(f"{col}_std")) for col in features)
-	.drop(["^[AG][xyz]_mean$", "^[AG][xyz]_std$"])
-)
-
-series_length: int = 42
-
-train_data_df = train_data_df.rolling(
-	index_column="time_order", group_by=["player_ID", "data_id"], period=f"{series_length - 1}i", closed="both"
-).agg(polars.all())
-
-train_data_df = (
-	train_data_df.with_columns(polars.col(features).list.get(0).name.suffix("_PAD"))
-	.with_columns(
-		polars.concat_list(polars.col(f"{col}_PAD").repeat_by(series_length - polars.col(col).list.len()), col).alias(
-			col
-		)
-		for col in features
-	)
-	.drop("^[AG][xyz]_PAD$")
-)
+train_data_df = train_data_df.group_by(["data_id", "player_ID"]).agg(polars.all())
 
 train_df = train_data_df.join(train_info_df, on="data_id").drop(["player_ID", "data_id", "time_order"])
 
@@ -62,126 +50,82 @@ dataset = Dataset(train_df.to_arrow())
 dataset = dataset.map(
 	lambda sample: {"data": torch.stack(tuple(torch.tensor(sample[col]) for col in features), dim=1)},
 	remove_columns=features,
-	num_proc=num_workers,
 )
 
 dataset = dataset.train_test_split(test_size=0.2)
 dataset = dataset.with_format("torch")
 
-train_dataloader = DataLoader(
-	dataset["train"],
-	batch_size=4,
-	num_workers=num_workers,
-	pin_memory=True,
-	multiprocessing_context="spawn",
-	persistent_workers=True,
-)
-validation_dataloader = DataLoader(
-	dataset["test"],
-	batch_size=4,
-	num_workers=num_workers,
-	pin_memory=True,
-	multiprocessing_context="spawn",
-	persistent_workers=True,
-)
+train_dataloader = DataLoader(dataset["train"])
+validation_dataloader = DataLoader(dataset["test"])
 
-num_epochs: int = 5
-learning_rate: float = 0.0001
+num_epochs = [20, 3, 25, 25]
+learning_rate: float = 1e-3
 
-model = LSTMClassifier(42, device="cuda")
+models = [
+	LSTMClassifier(input_size=6, lstm_hidden_size=16, lstm_num_layers=3, output_features=2, device="cuda"),
+	LSTMClassifier(input_size=6, lstm_hidden_size=32, lstm_num_layers=2, output_features=2, device="cuda"),
+	LSTMClassifier(input_size=6, lstm_hidden_size=16, lstm_num_layers=3, output_features=3, device="cuda"),
+	LSTMClassifier(input_size=6, lstm_hidden_size=16, lstm_num_layers=3, output_features=3, device="cuda"),
+]
 
 loss_fn = CrossEntropyLoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+optimizers = [torch.optim.AdamW(models[i].parameters(), lr=learning_rate, weight_decay=1e-4) for i in range(4)]
+learning_rate_scheduler = [ConstantLR(optimizers[i]) for i in range(4)]
 
-for i in range(num_epochs):
-	model.train()
-	training_loss: torch.Tensor = torch.empty(0)
-	for batch in tqdm(train_dataloader, desc=f"Train(Epoch {i}): ", colour="green"):
-		data = batch["data"].bfloat16().to(device)
-		label = tuple(batch[col].long().to(device) for col in labels)
+for m in range(4):
+	for i in range(num_epochs[m]):
+		models[m].train()
+		training_loss: torch.Tensor = torch.empty(0)
+		print(f"Learning Rate({labels[m]} classifier, Epoch {i}): {learning_rate_scheduler[m].get_last_lr()[0]}")
+		for batch in tqdm(train_dataloader, desc=f"Train({labels[m]} classifier, Epoch {i}): ", colour="green"):
+			data = batch["data"].to(dtype=dtype, device=device)
+			label = batch[labels[m]].to(dtype=torch.long, device=device)
 
-		optimizer.zero_grad()
-		outputs = model.forward(data)
-		loss = torch.tensor([loss_fn(outputs[j], label[j]) for j in range(4)], requires_grad=True).sum().unsqueeze(0)
-		training_loss = torch.cat((training_loss, loss))
-		loss.backward()
-		optimizer.step()
-	print(f"Training Loss(Epoch {i}): {training_loss.mean().item()}")
-	validation_loss: torch.Tensor = torch.empty(0)
-	predictions = [torch.empty(0) for _ in range(4)]
-	truths = [torch.empty(0) for _ in range(4)]
-	with torch.no_grad():
-		model.eval()
-		count = 0
-		for batch in tqdm(validation_dataloader, desc=f"Validation(Epoch {i}): ", colour="blue"):
-			data = batch["data"].bfloat16()
-			label = tuple(batch[col].long() for col in labels)
+			optimizers[m].zero_grad()
+			outputs = models[m].forward(data)
+			loss = loss_fn(outputs, label)
+			training_loss = torch.cat((training_loss, loss.unsqueeze(0).cpu()))
+			loss.backward()
+			optimizers[m].step()
+		print(f"Training Loss(Epoch {i}): {training_loss.mean().item()}")
+		validation_loss: torch.Tensor = torch.empty(0)
+		with torch.no_grad():
+			models[m].eval()
+			for batch in tqdm(
+				validation_dataloader, desc=f"Validation({labels[m]} classifier, Epoch {i}): ", colour="blue"
+			):
+				data = batch["data"].to(dtype=dtype, device=device)
+				label = batch[labels[m]].to(dtype=torch.long, device=device)
 
-			outputs = model.forward(data.to(device))
-			loss = (
-				torch.tensor([loss_fn(outputs[j], label[j].to(device)) for j in range(4)], requires_grad=True)
-				.sum()
-				.unsqueeze(0)
-			)
-			validation_loss = torch.cat((validation_loss, loss))
+				outputs = models[m].forward(data.to(device))
+				loss = loss_fn(outputs, label)
+				learning_rate_scheduler[m].step()
+				validation_loss = torch.cat((validation_loss, loss.unsqueeze(0).cpu()))
 
-			truths = [torch.cat((truths[i], label[i])) for i in range(4)]
-			predictions = [torch.cat((predictions[i], torch.max(outputs[i].cpu(), dim=-1)[0])) for i in range(4)]
-			count += 1
-	print(f"Validation Loss(Epoch {i}): {validation_loss.mean().item()}")
-	gender_score = roc_auc_score(truths[0].numpy(), predictions[0].numpy(), average="micro")
-	hold_racket_handed_score = roc_auc_score(truths[1].numpy(), predictions[1].numpy(), average="micro")
-	play_years_score = roc_auc_score(truths[2].numpy(), predictions[2].numpy(), average="micro", multi_class="ovr")
-	level_score = roc_auc_score(truths[3].numpy(), predictions[3].numpy(), average="micro", multi_class="ovr")
-	avg_score = (gender_score + hold_racket_handed_score + play_years_score + level_score) / 4
-	print(f"""
-        ROC AUC Score
-        Gender: {gender_score}
-        Hold racket handed: {hold_racket_handed_score}
-        Player years: {play_years_score}
-        Level: {level_score}
-        Average Score: {avg_score}
-        """)
+		print(f"Validation Loss(Epoch {i}): {validation_loss.mean().item()}")
 
 test_df = polars.read_csv(
 	f"{project_root}/data/test_data.csv", n_threads=num_workers, low_memory=True, rechunk=True, use_pyarrow=True
 )
 test_df = test_df.sort("data_id", "time_order").set_sorted("time_order")
 
-test_df = test_df.with_columns((polars.col(features) - polars.col(features).mean()) / polars.col(features).std())
-
-test_df = test_df.rolling(
-	index_column="time_order", group_by="data_id", period=f"{series_length - 1}i", closed="both"
-).agg(polars.all())
-
-test_df = (
-	test_df.with_columns(polars.col(features).list.get(0).name.suffix("_PAD"))
-	.with_columns(
-		polars.concat_list(polars.col(f"{col}_PAD").repeat_by(series_length - polars.col(col).list.len()), col).alias(
-			col
-		)
-		for col in features
-	)
-	.drop("^[AG][xyz]_PAD$")
-)
+test_df = test_df.group_by("data_id").agg(polars.all())
 
 test_df = test_df.drop("time_order")
 
 test_dataset = Dataset(test_df.to_arrow())
 
 test_dataset = test_dataset.map(
-	lambda sample: {"data": torch.stack(tuple(torch.tensor(sample[col]) for col in features), dim=1)},
+	lambda sample: {"data": torch.stack(tuple(torch.tensor(sample[col]) for col in features), dim=1) for i in range(4)},
 	remove_columns=features,
-	num_proc=num_workers,
 )
-
 test_dataset = test_dataset.with_format("torch", columns=["data"], output_all_columns=True)
 
 test_dataset = test_dataset.map(
-	lambda sample: {"output": model(sample.unsqueeze(0).to(device))},
-	input_columns=["data"],
-	remove_columns=["data"],
-	num_proc=num_workers,
+	lambda sample: {
+		"output": [models[i](sample["data"].unsqueeze(0).to(dtype=dtype, device=device)) for i in range(4)]
+	},
+	remove_columns=[f"data{i}" for i in range(4)],
 )
 
 test_dataset = test_dataset.map(
@@ -197,7 +141,6 @@ test_dataset = test_dataset.map(
 	},
 	input_columns=["output"],
 	remove_columns=["output"],
-	num_proc=num_workers,
 )
 
-test_dataset.to_csv("../data/result.csv", num_proc=num_workers)
+test_dataset.to_csv(f"{project_root}/data/result.csv")
--- src/tabletenis_player_classify_wave/Model.py
+++ src/tabletenis_player_classify_wave/Model.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional
 
 import torch
 
@@ -6,53 +6,44 @@ import torch
 class LSTMClassifier(torch.nn.Module):
 	def __init__(
 		self,
-		lstm_hidden_size: int,
-		lstm_num_layers: int = 1,
+		input_size,
+		lstm_hidden_size: int = 128,
+		lstm_num_layers: int = 2,
 		lstm_bias: bool = True,
-		lstm_dropout: float = 0.0,
 		lstm_proj_size: int = 0,
+		dropout: float = 0.3,
+		output_features: int = 2,
 		output_head_bias: bool = True,
 		device: Optional[str] = None,
-		dtype: torch.dtype = torch.bfloat16,
+		dtype: torch.dtype = torch.float32,
 	):
 		super(LSTMClassifier, self).__init__()
 
 		self.lstm = torch.nn.LSTM(
-			input_size=6,
+			input_size=input_size,
 			hidden_size=lstm_hidden_size,
 			num_layers=lstm_num_layers,
 			bias=lstm_bias,
 			batch_first=True,
-			dropout=lstm_dropout,
+			dropout=0,
 			bidirectional=False,
 			proj_size=lstm_proj_size,
 			device=device,
 			dtype=dtype,
 		)
+		self.dropout = torch.nn.Dropout(p=dropout)
 
-		self.gender_head = torch.nn.Linear(lstm_hidden_size, 2, bias=output_head_bias, device=device, dtype=dtype)
-		self.hold_racket_handed_head = torch.nn.Linear(
-			lstm_hidden_size, 2, bias=output_head_bias, device=device, dtype=dtype
-		)
-		self.play_years_head = torch.nn.Linear(lstm_hidden_size, 3, bias=output_head_bias, device=device, dtype=dtype)
-		self.level_head = torch.nn.Linear(lstm_hidden_size, 3, bias=output_head_bias, device=device, dtype=dtype)
-
-	def __call__(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-		gender_head_output, hold_racket_handed_head_output, play_years_head_output, level_head_output = self.forward(x)
-		return (
-			gender_head_output.softmax(dim=1).argmax(dim=1),
-			hold_racket_handed_head_output.softmax(dim=1).argmax(dim=1),
-			play_years_head_output.softmax(dim=1).argmax(dim=1),
-			level_head_output.softmax(dim=1).argmax(dim=1),
+		self.feature_head = torch.nn.Linear(
+			lstm_hidden_size, output_features, bias=output_head_bias, device=device, dtype=dtype
 		)
 
-	def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+	def __call__(self, x: torch.Tensor) -> torch.Tensor:
+		return self.forward(x).softmax(dim=1).argmax(dim=1)
+
+	def forward(self, x: torch.Tensor) -> torch.Tensor:
 		lstm_output, _ = self.lstm(x)
 		lstm_output = lstm_output[:, -1, :]
 
-		gender_head_output = self.gender_head(lstm_output)
-		hold_racket_handed_head_output = self.hold_racket_handed_head(lstm_output)
-		play_years_head_output = self.play_years_head(lstm_output)
-		level_head_output = self.level_head(lstm_output)
+		head_output = self.feature_head(lstm_output)
 
-		return gender_head_output, hold_racket_handed_head_output, play_years_head_output, level_head_output
+		return head_output
